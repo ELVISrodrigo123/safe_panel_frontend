f5da767dc60425254c5ed82020cc879047532b79
ELVISrodrigo123 <ec7902812@gmail.com>
2025-10-26 10:10:56 -0400

    correct username vpsj

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import HelloButton from "@/components/HelloButton";
+
 export default function Home() {
   return (
     <>
       <h1>HOLA</h1>
+      <HelloButton name="hola como estas" />
+      <h2>no puede ser</h2>
     </>
   );
 }
